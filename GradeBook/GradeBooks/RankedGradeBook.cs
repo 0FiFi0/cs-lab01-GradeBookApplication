ae19638b7433692dd87c7f7e49682a9107161f5d
0FiFi0 <115630892+0FiFi0@users.noreply.github.com>
2023-04-07 22:14:24 +0200

    Override RankedGradeBook's CalculateStudentStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -29,7 +29,28 @@ namespace GradeBook.GradeBooks
             if (percents < 60) return 'C';
             if (percents < 80) return 'D';
             else return 'F';
+        }
 
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5) 
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+                base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else 
+            {
+                base.CalculateStudentStatistics(name);
+            }
         }
     }
 }
